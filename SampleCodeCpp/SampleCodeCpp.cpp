c39b716cd1743e07d07c4fb981a5344c95ea4339
Afzaal Ahmad Zeeshan <afzaal.ahmad.zeeshan@hotmail.com>
2020-03-03 13:33:04 +0300

    Add project files.

---
+++ SampleCodeCpp/SampleCodeCpp.cpp
@@ -0,0 +1,16 @@
+// SampleCodeCpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class Relationship {
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
